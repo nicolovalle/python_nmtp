7f55d46f31e5becedd7182f109cca1fd79e6b52d
nicolovalle <vallenicolo@gmail.com>
2018-07-17 13:17:02 +0200

    prove_nicolo/

--- prove_nicolo/kurt_bootsrap.py
+++ prove_nicolo/kurt_bootsrap.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import moment
@@ -9,68 +12,72 @@ c1=0
 c2=0
 c3=0
 quantile=[]
+Teo={'gaus':0., 'unif':(-6./5), 'uniuni':(-6./7)}
 
-NN=30    #repeat the program NN times --> get NN bootstram distributions
+############# settings ####################
+NN=1    #repeat the program NN times --> get NN bootstram distributions
+Nboot=1000000   #bootstrap sample dimension (on 1000 data points)
+datapoints=500
+distr='uniuni'
+###########################################
 
-Nboot=2000   #bootstrap sample dimensin (on 1000 data points)
-datapoints=2000
+teo=Teo[distr]
+namefig='images/'+str(NN)+'volte_'+str(Nboot)+'bootstrap_su'+str(datapoints)+'dati_'+distr+'.png'
+
+def get_vector(s,n):
+    if (s=='gaus'):
+        return np.random.normal(0,1,n)
+    if (s=='unif'):
+        return np.random.random(n)
+    if (s=='uniuni'):
+        return np.power(np.random.random(n),2)
+        
 
 
 
+def bootstrap(Nboot_,datapoints_,distr_,want_vector):
+    VV=get_vector(distr_,datapoints_)
+    v=[]
+    for k in range(Nboot_):
+        if (k%1000==0):
+            print((Nboot_-k)/1000)
+        indici=np.random.randint(datapoints_,size=datapoints_)
+        boots=[VV[z] for z in indici]
+        curt=(moment(boots,4)/moment(boots,2)**2)-3
 
-#teo=-6./5 #uniforme
-#teo=-6./7 #uniforme**2
-teo=0.
+        v.append(curt)
+
+    if (want_vector):
+        return v, np.mean(v), np.std(v)
+    else:
+        return np.mean(v), np.std(v)
+    
+   
 
 for j in range(NN):
     print("----->j=",j)
 
-    #V_V=np.random.random(datapoints)
-    #VV=[v_v**2 for v_v in V_V]
-    VV=np.random.normal(0,1,datapoints)
     
-       
-    v = []
-
-    
-    
-    for k in range(Nboot):
-        if (k%1000==0):
-            print((Nboot-k)/1000)
-        
-        indici=np.random.randint(datapoints,size=datapoints)
-       
-        #print(s)
-
-
-        boots=[VV[z] for z in indici]
-        
-
-        curt=(moment(boots,4)/moment(boots,2)**2)-3
-        #print(curt)
-        
-
-        v.append(curt)
-    
-        
-    x=np.mean(v)
-    s=np.std(v)
-    #### plotting gaussian
+    #### plotting gaussian only if j=0
     if (j==0):
+        v,x,s=bootstrap(Nboot,datapoints,distr,True)
         nbin=int(np.sqrt(Nboot))
         if (NN>9):
             plt.subplot(2,1,1)
-        plt.title('Bootstrap distribution')
+        plttitle='Bootstrap distribution '+str(NN)+' '+str(Nboot)+' '+str(datapoints)+' '+distr
+        plt.title(plttitle)
         plt.hist(v,nbin,normed=True)
         minbin=x-4*s
         maxbin=x+4*s
         bins=[minbin+(maxbin-minbin)*ii/nbin for ii in range(nbin)]
         plt.plot(bins,1/(s*np.sqrt(2*np.pi))*np.exp(-(bins-x)**2/(2*s**2)),linewidth=2,color='r')
-        fx, fy = [teo,teo], [0,4]
+        fx, fy = [teo,teo], [0,1.05/(s*np.sqrt(2*np.pi))]
         plt.plot(fx,fy,color='g')
         textt=str(x)+"$\pm$"+str(s)
-        plt.text(-1.2,.025,textt)
-    #######################
+        textt='%7.4f'%(x)+' +/- '+'%7.4f'%(s) 
+        plt.text(teo-3.*s,1./(s*np.sqrt(2*np.pi)),textt)
+    else:
+        x,s=bootstrap(Nboot,datapoints,distr,False)
     X.append(x)
     S.append(s)
     quant=abs(x-teo)/s
@@ -80,25 +87,20 @@ for j in range(NN):
     
     print("In s/2: ",100.*c05/(j+1),"%\nIn s: ",100.*c1/(j+1),"%\nIn 2s:",100.*c2/(j+1),"%\nIn 3s:",100.*c3/(j+1),"%")
 
-## X è il vettore delle medie di Bootstrap
-## S è il vettore degli errori di Bootstrap
-print("\n")
-print("Teorico:",teo)
-print("Singolo esperimento:",X[0]," pm ",S[0])
-print("Media delle medie Bootstrap:",np.mean(X))
-print("RMS delle medie Bootstrap:",np.std(X))
-print("Media delle varianze Bootstrap:",np.mean(S))
-print("RMS delle varianze Bootstrap:",np.std(S))
+
+
 
 
 #####plotting quantiles
 if (NN>9):
     plt.subplot(2,1,2)
-    plt.title('Quantiles vs Standard Gaussian')
-    plt.hist(quantile,int(np.sqrt(NN)),normed=True)
+    #plt.text(1.5,0.7,'Quantiles vs Standard \n Gaussian')
+    summary='Teorico: %7.4f \nSingolo esperimento: %7.4f +/- %7.4f \nMedia delle medie Bootstrap: %7.4f \nRMS delle medie Bootstrap: %7.4f \nMedia delle varianze bootstrap: %7.4f \nRMS delle varianze Bootstrap: %7.4f'%(teo,X[0],S[0],np.mean(X),np.std(X),np.mean(S),np.std(S))
+    plt.text(1.5,0.25,summary)   
+    plt.hist(quantile,int(np.sqrt(NN)),normed=True,color='g')
     xq=[k/100. for k in range(400)]
     yq=[2/(np.sqrt(2*np.pi))*np.exp(-(QQ)**2/2) for QQ in xq]
     plt.plot(xq,yq,linewidth=2,color='b')
 
-
+plt.savefig(namefig)
 plt.show()
