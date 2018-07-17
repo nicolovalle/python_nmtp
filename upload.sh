fea04f03b396d71572ca6c36116dd605cbed8f06
nicolovalle <vallenicolo@gmail.com>
2018-07-17 09:43:06 +0200

    ...

--- upload.sh
+++ upload.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 git add $1
-echo ---------- ADDED $1 ------------
-if [ -z "$2" ]
-then
-    git commit -m $2
-else
+echo ---------- $1 ADDED ------------
+if [ -z "$2" ]; then
     git commit -m "..."
+    echo ------- COMMITTED WITH DEFAULT COMMENT --------
+else
+    git commit -m $2
+    echo ------- COMMITTED -----------   
 fi
 git pull --rebase
+echo ------- REBASE DONE ---------
 git push -u origin master
+echo ------- PUSHED ORIGIN -> MASTER ---------
