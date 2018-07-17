28a2c1efd92a71922e94e57dfd221dc5d7493cee
nicolovalle <vallenicolo@gmail.com>
2018-07-17 09:38:08 +0200

    ...

--- upload.sh
+++ upload.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 git add $1
-if [ -z "$2"]
+echo ---------- ADDED $1 ------------
+if [ -z "$2" ]
 then
     git commit -m $2
 else
-    git commit -m "No comment added"
+    git commit -m "..."
 fi
 git pull --rebase
 git push -u origin master
