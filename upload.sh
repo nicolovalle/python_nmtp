2d9ba567b6fc003a6e468b34196305b0a899dc91
nicolovalle <vallenicolo@gmail.com>
2018-07-17 09:43:45 +0200

    "upload.sh"

--- upload.sh
+++ upload.sh
@@ -3,7 +3,7 @@
 git add $1
 echo ---------- $1 ADDED ------------
 if [ -z "$2" ]; then
-    git commit -m "..."
+    git commit -m \"$1\"
     echo ------- COMMITTED WITH DEFAULT COMMENT --------
 else
     git commit -m $2
