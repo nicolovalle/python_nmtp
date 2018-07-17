059818af169effecc7c6d8d10e3084c9f46eaddf
nicolovalle <vallenicolo@gmail.com>
2018-07-17 09:59:51 +0200

    "upload.sh"

--- upload.sh
+++ upload.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-git add $1
-echo ---------- $1 ADDED ------------
-if [ -z "$2" ]; then
-    git commit -m \"$1\"
-    echo ------- COMMITTED WITH DEFAULT COMMENT --------
+if [ -z $1 ];then
+    echo
+    echo This program needs argument. Please use in this way:
+    echo $'\t' ./upload.sh miofile.py [\"Comment...\"]
+    echo $'\t' ./upload.sh mia_directory [\"Comment...\"]
+    echo
 else
-    git commit -m $2
-    echo ------- COMMITTED -----------   
+    git add $1
+    echo ---------- $1 ADDED ------------
+    if [ -z "$2" ]; then
+	git commit -m \"$1\"
+	echo ------- COMMITTED WITH DEFAULT COMMENT --------
+    else
+	git commit -m $2
+	echo ------- COMMITTED -----------   
+    fi
+    git pull --rebase
+    git push -u origin master
+    echo ------- DONE https://github.com/nicolovalle/python_nmtp ---------
 fi
-git pull --rebase
-git push -u origin master
-echo ------- DONE https://github.com/nicolovalle/python_nmtp ---------
+
